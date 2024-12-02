53e6ef8ecdcee0d5cb4d317c5c7e5b8cb817552b
CodeRacon <michael.buschmann.86@gmail.com>
2024-12-02 20:11:41 +0100

    feat(firebase): #1 add Firebase configuration and environment setup

--- nodes/src/app/app.config.ts
+++ nodes/src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore } from '@angular/fire/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { environment } from '../../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
