5b61bf0c57acdd490a7500a7e92097a79bcb56ba
CodeRacon <michael.buschmann.86@gmail.com>
2024-12-04 15:41:31 +0100

    chore(interface): #3 extended mindmap interface

--- nodes/src/app/interfaces/mindmap.interface.ts
+++ nodes/src/app/interfaces/mindmap.interface.ts
@@ -1,6 +1,9 @@
-export interface Node {
+export interface Node extends d3.SimulationNodeDatum {
   id: string;
   group: number;
+  collapsed?: boolean;
+  name?: string;
+  parent?: string;
 }
 
 export interface Link {
