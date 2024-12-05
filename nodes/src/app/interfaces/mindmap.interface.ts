942c7dee2d8f9491586ea558392a56bbffc1639a
CodeRacon <michael.buschmann.86@gmail.com>
2024-12-05 19:08:27 +0100

    refactor(interface): #3 enhance mindmap interface with d3 integration - Extends Node and Link interfaces with d3.js properties

--- nodes/src/app/interfaces/mindmap.interface.ts
+++ nodes/src/app/interfaces/mindmap.interface.ts
@@ -1,14 +1,22 @@
+import * as d3 from 'd3';
+
 export interface Node extends d3.SimulationNodeDatum {
   id: string;
   group: number;
   collapsed?: boolean;
   name?: string;
   parent?: string;
+  description?: string;
+  x?: number;
+  y?: number;
+  fx?: number | null;
+  fy?: number | null;
 }
 
-export interface Link {
+export interface Link extends d3.SimulationLinkDatum<Node> {
   source: string | Node;
   target: string | Node;
+  index?: number;
 }
 
 export interface MindmapData {
