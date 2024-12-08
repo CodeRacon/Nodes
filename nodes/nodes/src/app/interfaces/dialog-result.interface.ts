4aa19f9629acd02facd7e9363bdd27a7cd24325e
Michael Buschmann <146719762+CodeRacon@users.noreply.github.com>
2024-12-09 00:49:40 +0100

    Delete nodes/nodes directory

--- nodes/nodes/src/app/interfaces/dialog-result.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface DialogResult {
-  status: 'updated' | 'created' | 'deleted';
-  path?: {
-    mainTopic: string;
-    subTopic: string;
-    title: string;
-  };
-  kiContent?: string;
-}
