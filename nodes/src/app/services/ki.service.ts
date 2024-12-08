ac2f1bc5f5019a241d7470ae49fd3d16494f2311
CodeRacon <michael.buschmann.86@gmail.com>
2024-12-09 00:48:09 +0100

    doc: jsdoc

--- nodes/src/app/services/ki.service.ts
+++ nodes/src/app/services/ki.service.ts
@@ -16,10 +16,26 @@ export class KIService {
   constructor(private http: HttpClient) {}
 
   /**
-   * Sendet eine Anfrage an den OpenAI-Chat-Endpunkt und gibt das Antwort-Observable zurück.
+   * Sends a prompt to the AI service and returns the response as an Observable.
+   * The prompt is sent as a JSON payload with the following structure:
    *
-   * @param content - Der Inhalt der Nachricht, die an den OpenAI-Chat-Endpunkt gesendet werden soll.
-   * @returns Ein Observable, das die Antwort vom OpenAI-Chat-Endpunkt enthält.
+   * {
+   *   messages: [
+   *     {
+   *       role: 'system',
+   *       content: 'Allways use MarkDown syntax for formatting. Response detailed and well-structured. Allways format your answer with headings, lists, and/or other formatting elements if suitable. Do not use any other language than german.'
+   *     },
+   *     {
+   *       content: <content>,
+   *       role: 'user'
+   *     }
+   *   ],
+   *   model: 'mixtral'
+   * }
+   *
+   * The response from the AI service is returned as an Observable.
+   * @param content The prompt to send to the AI service.
+   * @returns An Observable containing the response from the AI service.
    */
   sendPrompt(content: string): Observable<any> {
     const headers = new HttpHeaders({
@@ -47,9 +63,11 @@ export class KIService {
   }
 
   /**
-   * Verarbeitet die Eingabe des Benutzers, sendet eine Anfrage an den OpenAI-Chat-Endpunkt und speichert die Antwort.
-   *
-   * Diese Methode wird aufgerufen, wenn der Benutzer eine Eingabe getätigt hat. Sie überprüft zunächst, ob die Eingabe nicht leer ist. Wenn dies der Fall ist, wird die `sendPrompt`-Methode aufgerufen, um eine Anfrage an den OpenAI-Chat-Endpunkt zu senden. Die Antwort wird dann in der `response`-Eigenschaft gespeichert. Wenn ein Fehler auftritt, wird eine Fehlermeldung in der `response`-Eigenschaft gespeichert.
+   * Sends a prompt to the AI service and updates the response property with the result.
+   * If the `kiInput` property is not empty, it sends the prompt to the AI service using the `sendPrompt` method.
+   * The response from the AI service is then stored in the `response` property.
+   * If there is an error sending the prompt, the `response` property is set to an error message.
+   * After the response is received, the `kiInput` property is reset to an empty string.
    */
   getAIResponse() {
     if (this.kiInput.trim()) {
